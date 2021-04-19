0064fb8d6a37b3e646510ea23e9777c77cd6ab8b
alwaysaditi <68003668+alwaysaditi@users.noreply.github.com>
2021-04-19 12:56:37 +0530

    Modified register page to add login button and added comments

--- startpage.py
+++ startpage.py
@@ -49,26 +49,30 @@ register_frame = Frame(frame, relief='raised',bg="pink",width=500,height=300)
 register_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 w = Label(register_frame, text ="Register!",bg="lightskyblue",fg="black")
 w.config(font=("Courier", 20))
-w.place( anchor="n",relx=0.5,rely=0.2)
+w.place( anchor="n",relx=0.5,rely=0.1)
 name = Label(register_frame,text ="First Name",bg="lightskyblue",fg="black") #Label box for name
 name.config(font=("Courier",10))
-name.place( anchor="n",relx=0.2,rely=0.4)
+name.place( anchor="n",relx=0.2,rely=0.3)
 entry1 = Entry(register_frame) #entry box for name
-entry1.place(anchor="n",relx=0.7,rely=0.4)
+entry1.place(anchor="n",relx=0.7,rely=0.3)
 flat = Label(register_frame,text ="Flat no.",bg="lightskyblue",fg="black") #Label box for flat no.
 flat.config(font=("Courier",10))
-flat.place( anchor="n",relx=0.2,rely=0.6)
+flat.place( anchor="n",relx=0.2,rely=0.5)
 entry2 = Entry(register_frame) #entry box for flat no.
-entry2.place(anchor="n",relx=0.7,rely=0.6)
+entry2.place(anchor="n",relx=0.7,rely=0.5)
 tenants = Label(register_frame,text ="Number of tenants",bg="lightskyblue",fg="black") #Label box for tenants
 tenants.config(font=("Courier",10))
-tenants.place( anchor="n",relx=0.2,rely=0.8)
+tenants.place( anchor="n",relx=0.2,rely=0.7)
 entry3 = Entry(register_frame) #entry box for tenants
-entry3.place(anchor="n",relx=0.7,rely=0.8)
+entry3.place(anchor="n",relx=0.7,rely=0.7)
 
 submitButton = Button(register_frame,text="Submit!",bg="lightblue",fg="black",relief="raised")
 submitButton.config(font=("Courier", 10)) #submit button on register page to submit data values after registering
-submitButton.place( anchor="n",relx=0.3,rely=0.9)
+submitButton.place( anchor="n",relx=0.2,rely=0.9)
+
+loginButton = Button(register_frame,text="Already have an account? Login",bg="lightblue",fg="black",relief="raised")
+loginButton.config(font=("Courier", 10)) #incase user already has an account he can login
+loginButton.place( anchor="n",relx=0.6,rely=0.9)
 
 
 
